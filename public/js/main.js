9514dcfd31bf3998a9d9f5a2233c3a75feb54d88
burd5 <austinburdette5@gmail.com>
2022-07-31 14:27:07 -0600

    edited delete/like

--- public/js/main.js
+++ public/js/main.js
@@ -2,7 +2,7 @@ const deleteText = document.querySelectorAll('.fa-trash')
 const thumbText = document.querySelectorAll('.fa-thumbs-up')
 
 Array.from(deleteText).forEach((element)=>{
-    element.addEventListener('click', deleteRapper)
+    element.addEventListener('click', deleteRestaurant)
 })
 
 Array.from(thumbText).forEach((element)=>{
